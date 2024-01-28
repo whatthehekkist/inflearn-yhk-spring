be24e9ba613d1f581b7c42490531485cd2b7a9c3
kyuing <kyykyu000@gmail.com>
2024-01-28 19:53:41 +0900

    v2.11: [refined] membership mgmt fe, Getmapping, Postmapping

--- _01/src/main/java/inflearn/yhk/_01/controller/MemberController.java
+++ _01/src/main/java/inflearn/yhk/_01/controller/MemberController.java
@@ -25,7 +25,7 @@ public class MemberController {
         return "members/createMemberForm"; // members/createMemberForm.html
     }
 
-    @PostMapping("members/new")
+    @PostMapping("/members/new")
     public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
