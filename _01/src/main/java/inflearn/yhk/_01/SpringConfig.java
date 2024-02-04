a81d5afe874124d3c03d501cef089061622c9d70
kyuing <kyykyu000@gmail.com>
2024-02-04 21:07:29 +0900

    v2.13: spring data access; h2, JDBC, DataSource DI

--- _01/src/main/java/inflearn/yhk/_01/SpringConfig.java
+++ _01/src/main/java/inflearn/yhk/_01/SpringConfig.java
@@ -1,21 +1,33 @@
 package inflearn.yhk._01;
 
+import inflearn.yhk._01.repository.JdbcMemberRepository;
 import inflearn.yhk._01.repository.MemberRepository;
 import inflearn.yhk._01.repository.MemoryMemberRepository;
 import inflearn.yhk._01.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
-    public MemberService memberService() {
+    public MemberService memberService(){
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
